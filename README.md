8a9327231aea10367024fa9f193661c2cb84e204
David Sheffield <sheffield.david@gmail.com>
2021-11-18 09:03:47 -0800

    readme and example binaries

---
+++ README.md
@@ -0,0 +1,1 @@
+David's pedagogical MIPS32 ISS with an LLVM-based binary translation engine
--- githash.cc
+++ githash.cc
@@ -1,1 +1,1 @@
-const char *githash = "HEAD";
+const char *githash = "358cf85cec720c1cf2e159691fe53e365ef01348";
